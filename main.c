fdc90e7881613692d947a4794ce1fa95cba6f120
Emma Harrison <eharriso1@ltu.edu>
2026-04-25 19:02:59 -0400

    Upload initial files

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "../../MRE_lib/F_cpu_lib.h"
+#include <avr/io.h>
+#include "../../MRE_lib/Serial_lib2.h"
+
+int main(void)
+{
+	MCUCR |= (1 << JTD);
+	MCUCR |= (1 << JTD);
+
+	DDRC |= (1 << PINC7);
+
+	PORTC |= (1 << PINC7);
+	_delay_ms(1000);
+	PORTC &= ~(1 << PINC7);
+
+	USART_vInit();
+
+	while (1)
+	{
+		char decision = USART_vReceiveByte();
+
+		if (decision == '1')
+		{
+			PORTC |= (1 << PINC7);
+		}
+		else if (decision == '0')
+		{
+			PORTC &= ~(1 << PINC7);
+		}
+	}
+}
