67b870677ec9fce6ccb369347907b0641d480893
Emma Harrison <eharriso1@ltu.edu>
2026-04-25 19:06:16 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,111 @@
+/*
+ * Final_Project_UR1.c
+ *
+ * Created: 4/25/2026 12:50:55 PM
+ * Author : eharriso1
+ */ 
+
+
 #include "../../MRE_lib/F_cpu_lib.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "../../MRE_lib/LCD_lib.h"
+#include "../../MRE_lib/A2D_lib.h"
 #include "../../MRE_lib/Serial_lib2.h"
+#include "../../MRE_lib/Motor_PWM_lib.h"
+
 
 int main(void)
 {
 	MCUCR |= (1 << JTD);
 	MCUCR |= (1 << JTD);
-
-	DDRC |= (1 << PINC7);
-
-	PORTC |= (1 << PINC7);
-	_delay_ms(1000);
-	PORTC &= ~(1 << PINC7);
-
+	LCD_init();
+	LCDGoToPosition(1,1);
+	char array[20];
+	char decision;
+	sprintf(array, "Hello World!!");
+	LCDSendString(array);
+	_delay_ms(2000);
+	LCDClearScreen();
+	//uint8_t serial_char_in;
+	//char serial_char_array_out[20];
+	DDRC |= 1 << PINC2;
+	//sprintf(array, "Emma Harrison");
+	//LCDSendString(array);
+	//_delay_ms(2000);
+	AD_init();
 	USART_vInit();
-
-	while (1)
-	{
-		char decision = USART_vReceiveByte();
-
-		if (decision == '1')
+	sei();
+	HBridgeInit(); // define ports and set up 16 bit timer TCNT1
+	//LCDClearScreen();
+	DDRA &= ~((1<<DDA0)|(1<<DDA1));
+	DDRB |= (1<<PINB7) | (1<<PINB6) | (1<<PINB5) | (1<<PINB4);
+	uint8_t left = 0;
+	uint8_t right = 1;
+	uint8_t lspeed;
+	uint8_t rspeed; 
+	uint8_t forward = 0;
+	uint8_t reverse = 1;
+    /* Replace with your application code */
+    while (1) 
+    {
+		decision = USART_vReceiveByte();
+		
+		sprintf(array, "Emma Harrison");
+		LCDSendString(array);
+		
+		 //0 -> STOP
+		 //1 -> FORWARD
+		 //2 -> TURN RIGHT
+		 //3 -> TURN LEFT
+		 
+		 //HBridgeCommand(uint8_t side, uint8_t speed, uint8_t direction);
+		 // where side 0 = left motor, 1 = right
+		 // speed = 0-100
+		 // direction 0 = forward, 1 = reverse
+		if(decision == '0') //STOP
 		{
-			PORTC |= (1 << PINC7);
+			lspeed = 0;
+			rspeed = 0;
+			
+			HBridgeCommand(left,lspeed,forward);
+			//_delay_ms(1000);
+			HBridgeCommand(right,rspeed,forward);
+			_delay_ms(10);
 		}
-		else if (decision == '0')
+		else if(decision == '1') //FORWARD
 		{
-			PORTC &= ~(1 << PINC7);
+			lspeed = 75;
+			rspeed = 75;
+			
+			HBridgeCommand(left,lspeed,forward);
+			//_delay_ms(1000);
+			HBridgeCommand(right,rspeed,forward);
+			_delay_ms(10);
 		}
-	}
+		else if(decision == '2') //TURN RIGHT
+		{
+			lspeed = 75;
+			rspeed = 60;
+			
+			HBridgeCommand(left,lspeed,forward);
+			//_delay_ms(1000);
+			HBridgeCommand(right,rspeed,forward);
+			_delay_ms(10);
+		}
+		else if(decision == '3') //TURN LEFT
+		{
+			lspeed = 60;
+			rspeed = 75;
+			
+			HBridgeCommand(left,lspeed,forward);
+			//_delay_ms(1000);
+			HBridgeCommand(right,rspeed,forward);
+			_delay_ms(10);			
+		}
+		
+    }
 }
+
